5b4f450eefacf8f54a580023f2b550b10b55561b
BuildTools <unconfigured@null.spigotmc.org>
2023-06-15 12:04:22 +0300

    *

---
+++ build/static/js/15.7884ba4b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[15],{495:function(s,e,n){"use strict";n.r(e);n(0);var t=n(5);e.default=function(){return Object(t.jsx)("div",{children:"dashboard component works"})}}}]);
+//# sourceMappingURL=15.7884ba4b.chunk.js.map
